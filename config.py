1a0028b93b1063f9f53647e146e805a445f6a627
hemant67890 <godarasubhash337@gmail.com>
2025-01-06 00:39:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 21537501
 
 API_HASH = os.environ.get("API_HASH", "02d8ef0eae2926ec4fd0cbff05ec0737")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7013832215:AAGUP5D7APrtiqpWy5d4VCsGMsnvFNSSDBA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7836714507:AAE0uMDhthtcFEPoO96xxUxXmeyW6Obz5tU")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
